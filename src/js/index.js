989f20db081d18f51c68d90470dd3c88ce67bd45
Valerii Vitenko <valero2313@gmail.com>
2021-05-13 01:03:08 +0300

    first 20 tasks

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,171 @@
-"use strict";
-// Enter your code below:
+'use strict';
+// Do you think you can handle this?
+
+// ==================== 10 ====================
+function makeMessage(name, price) {
+  const message = `You picked ${name}, price per item is ${price} credits`;
+  return message;
+}
+
+console.log(makeMessage('Radar', 6150));
+console.log(makeMessage('Scanner', 3500));
+console.log(makeMessage('Reactor', 8000));
+
+// =================== 11 =====================
+function calculateTotalPrice(orderedQuantity, pricePerItem) {
+  const totalPrice = orderedQuantity * pricePerItem;
+
+  return totalPrice;
+}
+
+console.log(calculateTotalPrice(5, 100));
+console.log(calculateTotalPrice(8, 60));
+console.log(calculateTotalPrice(3, 400));
+console.log(calculateTotalPrice(1, 3500));
+console.log(calculateTotalPrice(12, 70));
+
+// =================== 12 ======================
+function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+  const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+  const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+  return message;
+}
+
+console.log(makeOrderMessage(2, 100, 50));
+console.log(makeOrderMessage(4, 300, 100));
+console.log(makeOrderMessage(10, 70, 200));
+
+// ==================== 13 ======================
+function isAdult(age) {
+  const passed = age >= 18;
+
+  return passed;
+}
+
+console.log(isAdult(20));
+console.log(isAdult(14));
+console.log(isAdult(8));
+console.log(isAdult(37));
+
+// ==================== 14 ======================
+function isValidPassword(password) {
+  const SAVED_PASSWORD = 'jqueryismyjam';
+  const isMatch = password === SAVED_PASSWORD;
+
+  return isMatch;
+}
+
+console.log(isValidPassword('mangodab3st'));
+console.log(isValidPassword('kiwirul3z'));
+console.log(isValidPassword('jqueryismyjam'));
+
+// ==================== 15 ======================
+function checkAge(age) {
+  let message;
+
+  if (age >= 18) {
+    message = 'You are an adult';
+  } else {
+    message = 'You are a minor';
+  }
+
+  return message;
+}
+
+console.log(checkAge(20));
+console.log(checkAge(8));
+console.log(checkAge(14));
+console.log(checkAge(38));
+
+// ==================== 16 ======================
+function checkStorage(available, ordered) {
+  let message;
+  if (available < ordered) {
+    message = 'Not enough goods in stock!';
+  } else {
+    message = 'Order is processed, our manager will contact you.';
+  }
+  return message;
+}
+
+console.log(checkStorage(100, 50));
+console.log(checkStorage(100, 130));
+console.log(checkStorage(200, 20));
+
+// ==================== 17 ======================
+let a = 5;
+let b = 10;
+let c = 15;
+let d = 20;
+
+a += 2;
+b -= 4;
+c *= 3;
+d /= 10;
+
+// ==================== 18 ======================
+function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+  let message;
+  const totalPrice = pricePerDroid * orderedQuantity;
+  if (totalPrice > customerCredits) {
+    message = 'Insufficient funds!';
+  } else if (totalPrice <= customerCredits) {
+    customerCredits -= totalPrice;
+    message = `You ordered ${orderedQuantity} droids, you have ${customerCredits} credits left`;
+  }
+  return message;
+}
+
+console.log(makeTransaction(3000, 5, 23000));
+console.log(makeTransaction(1000, 3, 15000));
+console.log(makeTransaction(5000, 10, 8000));
+console.log(makeTransaction(2000, 8, 10000));
+console.log(makeTransaction(500, 10, 5000));
+
+// ==================== 19 ======================
+function checkPassword(password) {
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+  let message;
+
+  if (password === null) {
+    message = 'Canceled by user!';
+  } else if (password === 'jqueryismyjam') {
+    message = 'Welcome!';
+  } else {
+    message = 'Access denied, wrong password!';
+  }
+  return message;
+}
+
+console.log(checkPassword('mangohackzor'));
+console.log(checkPassword(null));
+console.log(checkPassword('polyhax'));
+console.log(checkPassword('jqueryismyjam'));
+
+// ==================== 20 ======================
+function checkStorage(available, ordered) {
+  let message;
+  if (ordered === 0) {
+    message = 'There are no products in the order!';
+  } else if (ordered > available) {
+    message = 'Your order is too large, there are not enough items in stock!';
+  } else {
+    message = 'The order is accepted, our manager will contact you';
+  }
+  return message;
+}
+
+console.log(checkStorage(100, 50));
+console.log(checkStorage(100, 130));
+console.log(checkStorage(70, 0));
+console.log(checkStorage(200, 20));
+console.log(checkStorage(200, 250));
+console.log(checkStorage(150, 0));
+
+// ==================== 21 ======================
+// ==================== 22 ======================
+// ==================== 23 ======================
+// ==================== 24 ======================
+// ==================== 25 ======================
+// ==================== 26 ======================
