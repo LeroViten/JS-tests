63ff26e53ffa89184f16b15ebe817b62c31dac9d
Valerii Vitenko <valero2313@gmail.com>
2021-07-05 19:26:10 +0300

    module8: 1st webinar tasks

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,152 @@
 'use strict';
 // Enter your code below:
+const container = document.querySelector('.js-container');
+container.addEventListener('click', onClick);
 
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
-// * ======================== 1 ===========================
+function onClick(evt) {
+  if (evt.target.nodeName !== 'BUTTON') {
+    return;
+  }
+  console.log(evt.target.textContent);
+}
+
+// adding buttons by pressing 'take me there' btn;
+const addBtnRef = document.querySelector('.js-add-btn');
+let labelCounter = 6;
+
+addBtnRef.addEventListener('click', onAddBtnClick);
+
+function onAddBtnClick() {
+  const btn = document.createElement('button');
+  btn.textContent = `Button ${labelCounter}`;
+  btn.type = 'button';
+  btn.classList.add('js-btn');
+
+  container.appendChild(btn);
+  labelCounter += 1;
+}
+//! -------------- TAGS FACTORY ------------------
+
+// const tagsContainer = document.querySelector('.js-tags');
+// tagsContainer.addEventListener('click', onTagsContainerClick);
+// let selectedTag = null;
+
+// //! selecting one tag
+// function onTagsContainerClick(event) {
+//   if (event.target.nodeName !== 'BUTTON') {
+//     return;
+//   }
+
+//   const currentActiveBtn = document.querySelector('.tags__btn--active');
+//   if (currentActiveBtn) {
+//     currentActiveBtn.classList.remove('tags__btn--active');
+//   }
+//   //? or the same:
+//   //? currentActiveBtn?.classList.remove('tags__btn--active');
+
+//   const nextActiveBtn = event.target;
+//   nextActiveBtn.classList.add('tags__btn--active');
+//   selectedTag = nextActiveBtn.dataset.value;
+//   console.log(selectedTag);
+// }
+
+//! selecting multiple tags:
+
+const tagsContainer = document.querySelector('.js-tags');
+tagsContainer.addEventListener('click', onTagsContainerClick);
+const selectedTags = new Set(); // stores unique values of primitives
+function onTagsContainerClick(event) {
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  }
+
+  const btn = event.target;
+  const tag = btn.dataset.value;
+  const isActiveBtn = btn.classList.contains('tags__btn--active');
+
+  if (isActiveBtn) {
+    selectedTags.delete(tag);
+  } else {
+    selectedTags.add(tag);
+  }
+
+  btn.classList.toggle('tags__btn--active');
+  console.log(selectedTags);
+}
+
+//! ============== COLOR PICKER FACTORY =========
+
+const colors = [
+  { hex: '#f44336', rgb: '244,67,54' },
+  { hex: '#e91e63', rgb: '233,30,99' },
+  { hex: '#9c27b0', rgb: '156,39,176' },
+  { hex: '#673ab7', rgb: '103,58,183' },
+  { hex: '#3f51b5', rgb: '63,81,181' },
+  { hex: '#2196f3', rgb: '33,150,243' },
+  { hex: '#00bcd4', rgb: '0,188,212' },
+  { hex: '#009688', rgb: '0,150,136' },
+  { hex: '#4caf50', rgb: '76,175,80' },
+  { hex: '#ffeb3b', rgb: '255,235,59' },
+  { hex: '#ff9800', rgb: '255,152,0' },
+  { hex: '#795548', rgb: '121,85,72' },
+  { hex: '#607d8b', rgb: '96,125,139' },
+];
+
+const paletteContainer = document.querySelector('.js-palette');
+const cardsMarkup = createColorCardsMarkup(colors);
+
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+paletteContainer.addEventListener('click', onPaletteContainerClick);
+
+function createColorCardsMarkup(colors) {
+  return colors
+    .map(({ hex, rgb }) => {
+      return `
+    <div class="color-card">
+     <div><div><div> <div
+     class="color-swatch"
+     data-hex="${hex}"
+     data-rgb="${rgb}"
+     style="background-color: ${hex}"
+   ></div></div></div></div>
+      <div class="color-meta">
+        <p>HEX: ${hex}</p>
+        <p>RGB: ${rgb}</p>
+      </div>
+    </div>
+    `;
+    })
+    .join('');
+}
+
+function onPaletteContainerClick(evt) {
+  const isColorSwatchEl = evt.target.classList.contains('color-swatch');
+
+  if (!isColorSwatchEl) {
+    return;
+  }
+
+  const swatchEl = evt.target;
+  const parentColorCard = swatchEl.closest('.color-card');
+
+  removeActiveCardClass();
+  addActiveCardClass(parentColorCard);
+  setBodyBgColor(swatchEl.dataset.hex);
+}
+
+function setBodyBgColor(color) {
+  document.body.style.backgroundColor = color;
+}
+
+function removeActiveCardClass() {
+  const currentActiveCard = document.querySelector('.color-card.is-active');
+
+  if (currentActiveCard) {
+    currentActiveCard.classList.remove('is-active');
+  }
+}
+
+function addActiveCardClass(card) {
+  card.classList.add('is-active');
+}
